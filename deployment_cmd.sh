5438607991493a5f7210e664cfb10e00239b1fad
Sarbadal Pal <sarbadal.pal@omc.com>
2026-07-09 18:16:10 +0400

    add deployment cli

---
+++ deployment_cmd.sh
@@ -0,0 +1,6 @@
+python deployment.py \
+  --function-name sales-dashboard-template-b \
+  --project-id YOUR_GCP_PROJECT \
+  --bucket-name YOUR_STATIC_BUCKET \
+  --region us-central1 \
+  --allow-unauthenticated
